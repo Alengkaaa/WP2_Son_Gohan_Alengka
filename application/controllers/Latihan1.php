f87c943f1e41bf346ca04a2d09553772058222e5
Alengkaaa <alengkahagen@gmail.com>
2023-03-22 20:12:32 +0700

    Tugas Pertemuan 2

---
+++ application/controllers/Latihan1.php
@@ -0,0 +1,18 @@
+<?php
+// CONTOH 01 CONTROLLER
+class Latihan1 extends CI_Controller
+{
+ public function index()
+ {
+ echo "Selamat Datang.. selamat belajar Web Programming";
+ //$this->load->view('view-latihan1');
+ }
+ public function penjumlahan($n1, $n2)
+ {
+ $this->load->model('Model_latihan1');
+ $data['nilai1'] = $n1;
+ $data['nilai2'] = $n2;
+ $data['hasil'] = $this->Model_latihan1->jumlah($n1, $n2);
+ $this->load->view('view-latihan1', $data);
+ }
+}
