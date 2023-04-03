4093888f926cc535cdab716c29a5bcc3f9a6fb8f
Alengkaaa <alengkahagen@gmail.com>
2023-04-03 14:47:04 +0700

    perubahan pada header ada kesalahan

--- application/views/v_header.php
+++ application/views/v_header.php
@@ -1,14 +1,14 @@
 <!DOCTYPE html>
 <html>
 <head>
- <meta charset=‛utf-8‛>
+ <meta charset="utf-8">
  <title>Web Prog II | Merancang Template sederhana dengan 
 codeigniter</title>
 <link rel="stylesheet" type="text/css" 
 href="http://localhost/pustaka-booking/assets/css/stylebuku.css">
 </head>
 <body>
- <div id=‛wrapper‛>
+ <div id="wrapper">
  <header>
  <hgroup>
  <h1>RentalBuku.net</h1>
